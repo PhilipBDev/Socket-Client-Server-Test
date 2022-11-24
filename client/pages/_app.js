ac61c3c5702dae5415279aa125c7940b50f2acfc
Philip <philipbdev@gmail.com>
2022-11-24 12:10:28 -0500

    Functional socket.io app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
